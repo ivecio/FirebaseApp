082988d362c5a1d0f02ab1af83c0b92957e7b7e2
Ivecio <Ivecio>
2016-09-03 18:40:21 -0300

    Inluído produto e nova classe Java

--- app/src/main/java/firebaseapp/ivecio/com/firebaseapp/MainActivity.java
+++ app/src/main/java/firebaseapp/ivecio/com/firebaseapp/MainActivity.java
@@ -11,13 +11,27 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference(); //recupera a instância do banco de dados - altera direto no BD
                                                                                                     // o reference pega o "root" do Firebase, a raiz principal
     private DatabaseReference usuarioReferencia = databaseReferencia.child("usuarios");
+    private DatabaseReference produtoReferencia = databaseReferencia.child("produtos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usuarioReferencia.child("001").child("nome").setValue("Ivecio Filho");
+        Usuario usuario = new Usuario();
+        usuario.setNome("Guilherme");
+        usuario.setSobrenome("Favretto");
+        usuario.setIdade(11);
+        usuario.setSexo("masculino");
+
+        usuarioReferencia.child("003").setValue(usuario);
+
+        Produto produto = new Produto();
+        produto.setCor("Amarelo");
+        produto.setDecricao("Produto amarelo");
+        produto.setFabricante("Empresa Amarela");
+
+        produtoReferencia.child("001").setValue(produto);
 
     }
 }
---
+++ app/src/main/java/firebaseapp/ivecio/com/firebaseapp/Produto.java
@@ -0,0 +1,36 @@
+package firebaseapp.ivecio.com.firebaseapp;
+
+
+public class Produto {
+
+    private String decricao;
+    private String cor;
+    private String fabricante;
+
+    public Produto() {
+    }
+
+    public String getDecricao() {
+        return decricao;
+    }
+
+    public void setDecricao(String decricao) {
+        this.decricao = decricao;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+}
